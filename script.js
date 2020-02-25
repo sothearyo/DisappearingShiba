67aec3d20497b9c8ef94b4dfed95f93eae56258f
Sotheary Ouk <sothearyo@gmail.com>
2020-02-25 15:48:08 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('button').click(function(){
+        $( this ).hide();
+    })
+})
+
+$(document).ready(function(){
+    $('#restore').click(function(){
+        $('button').show();
+    })
+})
